b6a79d322076ce0402c16a27613a0c455e948cb9
Naswa Nida <naswanida3@gmail.com>
2025-11-20 16:45:39 +0700

    update structure project

--- cms/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once '../app/config/koneksi.php';
-require_once '../app/cms/controllers/AuthController.php';
-
-$authController = new AuthController();
-$authController->login($conn);
