89e4eb42e3db57f625ce7479ab9c4d4286845add
Naswa Nida <naswanida3@gmail.com>
2025-12-14 19:20:49 +0700

    update footer

--- app/cms/views/layout/footer.php
+++ app/cms/views/layout/footer.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col my-1">
                     <p class="m-0">
-                        Lab BA &copy; <?php echo date('Y'); ?> Made with ❤️ by Your Team
+                        Lab BA &copy; <?php echo date('Y'); ?> Made with ❤️
                     </p>
                 </div>
             </div>
